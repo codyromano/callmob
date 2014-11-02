8e4b65dd09c82eca621cb1b7d7cd6357d590734e
Firoze Rakib <frakib@mobiquityinc.com>
2014-11-02 12:02:15 -0500

    weekend + daylight

--- main.js
+++ main.js
@@ -16,6 +16,15 @@
 	function addLeadZero (int) {
 		return int < 10 ? '0' + int : int; 
 	}
+	
+	function isWeekend() {
+		var today = new Date();
+		if (today.getDay() == 6 || today.getDay() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	function Office (name, utcOffset, regularHours) {
 		var regularHours = regularHours || [9,17];
@@ -33,26 +42,33 @@
 		this.open = this.hours >=regularHours[0] && this.hours < regularHours[1]; 
 
 		this.status = (function() {
-			if ((this.hours >= 7 && this.hours < regularHours[0]) || (this.hours > regularHours[1] && this.hours <= 22)) {
-				return 'Discretion';
+			if (isWeekend()) {
+				return 'Closed';
 			} else {
-				if (this.open) {
-					return 'Open';
+				if ((this.hours >= 7 && this.hours < regularHours[0]) || (this.hours > regularHours[1] && this.hours <= 22)) {
+					return 'Discretion';
 				} else {
-					return 'Closed';
+					if (this.open) {
+						return 'Open';
+					} else {
+						return 'Closed';
+					}
 				}
 			}
 		});
 		
 		this.mayCall = (function() {
-			
-			if (this.open) {
-				return 'Yes';
+			if (isWeekend()) {
+				return "No Way! It's the weekend!";
 			} else {
-				if (this.hours < 7 || this.hours > 22) {
-					return "No Way!";
+				if (this.open) {
+					return 'Yes';
 				} else {
-					return "Use Discretion";
+					if (this.hours < 7 || this.hours > 22) {
+						return "No Way!";
+					} else {
+						return "Use Discretion";
+					}
 				}
 			}
 		});
@@ -76,23 +92,28 @@
 	(function getTimeInfo () {
 		
 		var GMT = 0;
+		
 		var EDT = GMT - 4;
 		var CDT = GMT - 5;
 		var PDT = GMT - 7;
 		
+		var EST = GMT - 5;
+		var CST = GMT - 6;
+		var PST = GMT - 8;
+		
 		var offices = [
-			new Office('Dallas', CDT),
-			new Office('Philadelphia', EDT),
-			new Office('Providence', EDT),
-			new Office('New York', EDT), 
-			new Office('Wellesley', EDT),
-			new Office('Waltham', EDT),
-			new Office('Gainesville', EDT),
-			new Office('San Francisco', PDT),
-			new Office('San Jose', PDT),
+			new Office('Dallas', CST),
+			new Office('Philadelphia', EST),
+			new Office('Providence', EST),
+			new Office('New York', EST), 
+			new Office('Wellesley', EST),
+			new Office('Waltham', EST),
+			new Office('Gainesville', EST),
+			new Office('San Francisco', PST),
+			new Office('San Jose', PST),
 			new Office('Amsterdam', GMT + 2),
 			new Office('Ahmedabad', GMT + 5.5, [10,19]),
-			new Office('Atlanta', EDT)
+			new Office('Atlanta', EST)
 		].sort(function (a, b) {
 			  var textA = a.name.toUpperCase();
     		  var textB = b.name.toUpperCase();
